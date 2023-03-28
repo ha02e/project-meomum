187b84b5221b61f1270d13031ba366831e94289d
DESKTOP-CH4GGAI\gram <wkdtn134@naver.com>
2023-03-29 02:21:23 +0900

    워크스페이스 생성

---
+++ meomum/src/main/webapp/js/request.js
@@ -0,0 +1,34 @@
+var XHR = null;
+
+function getXHR(){
+	if(window.ActiveXObject){
+		return new ActiveXObject('Msxml2.XMLHTTP');
+	}else if(window.XMLHttpRequest){
+		return new XMLHttpRequest();
+	}else{
+		return null;
+	}
+}
+
+function sendRequest(url,param,method,callback){
+	XHR = getXHR();
+
+	var newMethod = method?method:'GET';
+	if(newMethod != 'GET' && newMethod != 'POST'){
+		newMethod = 'GET';
+	}
+
+	var newParam = (param == null || param == '')?null:param;
+	
+	var newUrl =  url;
+	if(newMethod == 'GET' && newParam != null){
+		newUrl = newUrl+'?'+newParam;
+	}
+	
+	XHR.onreadystatechange = callback;
+
+	XHR.open(newMethod,newUrl,true);
+ 	XHR.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	XHR.send(newMethod == 'POST'?newParam:null); 
+	
+}
