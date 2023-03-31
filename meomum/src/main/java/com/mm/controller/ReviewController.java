dddf5dbd238368a6c17c3abd66920e49d4d83960
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-03-31 11:32:18 +0900

    230331 - review 작성 테스트 초기화

--- meomum/src/main/java/com/mm/controller/ReviewController.java
+++ meomum/src/main/java/com/mm/controller/ReviewController.java
@@ -1,20 +1,13 @@
 package com.mm.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.mm.review.model.ReviewDAO;
-import com.mm.review.model.ReviewDTO;
-import com.mm.review.service.ReviewService;
 
 @Controller
 public class ReviewController {
 
-	@Autowired
-	private ReviewService reviewService;
 	
 	@RequestMapping("/myReview.do")
 	public String myReview() {
