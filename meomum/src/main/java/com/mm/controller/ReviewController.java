fe7b1de4ea1a47e7e3525f566e6677601c401946
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-04-02 22:32:25 +0900

    230402 - 리뷰작성 이미지 db 저장 1차 완료

--- meomum/src/main/java/com/mm/controller/ReviewController.java
+++ meomum/src/main/java/com/mm/controller/ReviewController.java
@@ -1,14 +1,16 @@
 package com.mm.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
+import java.io.*;
+import java.util.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -21,7 +23,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.google.gson.JsonObject;
 import com.mm.review.model.ReviewDTO;
 import com.mm.review.service.ReviewService;
 
@@ -32,6 +33,11 @@ public class ReviewController {
 	@Autowired
 	private ReviewService reviewService;
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Resource(name="uploadPath")
+	private String uploadPath;
+	
 	
 	@RequestMapping("/myReview.do")
 	public String myReview() {
@@ -64,37 +70,59 @@ public class ReviewController {
 		return mav;
 	}
 	
-	/*
-	@RequestMapping(value="/reviewWrite.do", method = RequestMethod.POST)
-    public ModelAndView reviewWriteSubmit(ReviewDTO dto) {
-		int result=reviewService.reviewInsert(dto);
-		String msg=result>0?"리뷰 등록 완료되었습니다.":"리뷰 등록에 실패하였습니다.";
-		String link = result>0?"myReview.do":"reviewWrite.do";
+	/** ckeditor 파일업로드 */
+	@RequestMapping(value="/review/ckUpload", method=RequestMethod.POST)
+	public void ckeditorUpload(HttpServletRequest req,
+	          				HttpServletResponse res,
+	          				@RequestParam MultipartFile upload)throws Exception {
 		
-		ModelAndView mav=new ModelAndView();
-		mav.addObject("msg", msg);
-		mav.addObject("link", link);
-		mav.setViewName("/msg");
-		
-		return mav;
-    }
-	
-	
-	@RequestMapping(value="SummerNoteImageFile" , method = RequestMethod.POST)
-	public @ResponseBody JsonObject SummerNoteImageFile(@RequestParam("file") MultipartFile file) {
-		JsonObject jsonObject = reviewService.SummerNoteImageFile(file);
-		System.out.println(jsonObject);
-		return jsonObject;
+		logger.info("post CKEditor img upload");
+	          					 
+		// 랜덤 문자 생성
+		UUID uid = UUID.randomUUID();
+	          					 
+		OutputStream out = null;
+		PrintWriter printWriter = null;
+	          					   
+		// 인코딩
+	    res.setCharacterEncoding("utf-8");
+	    res.setContentType("text/html;charset=utf-8");
+	          					 
+	    try {
+	          					  
+	    	String fileName = upload.getOriginalFilename();  // 파일 이름 가져오기
+	    	byte[] bytes = upload.getBytes();
+	          					  
+	        // 업로드 경로
+	        String ckUploadPath = uploadPath + File.separator + "ckUpload" + File.separator + uid + "_" + fileName;
+	          					  
+	        out = new FileOutputStream(new File(ckUploadPath));
+	        out.write(bytes);
+	        out.flush();  // out에 저장된 데이터를 전송하고 초기화
+	          					  
+	        String callback = req.getParameter("CKEditorFuncNum");
+	        printWriter = res.getWriter();
+	        String fileUrl = "/ckUpload/" + uid + "_" + fileName;  // 작성화면
+	          					  
+	        // 업로드시 메시지 출력
+	      	printWriter.println("<script type='text/javascript'>"
+	      			+ "window.parent.CKEDITOR.tools.callFunction("
+	      			+ callback+",'"+ fileUrl+"','이미지를 업로드하였습니다.')"
+	      			+"</script>");
+	          					  
+	    	printWriter.flush();
+	          					  
+	        } catch (IOException e) { e.printStackTrace();
+	        } finally {
+	        try {
+	        	if(out != null) { out.close(); }
+	          	if(printWriter != null) { printWriter.close(); }
+	          	} catch(IOException e) { e.printStackTrace(); }
+	        }
+	          					 
+	       	return;
+	       	
 	}
-
-	
-
-	@PostMapping("/review/article")
-	public ReviewDTO<> saveReview(@RequestBody @Valid Board board, @PrincipalDetail principal) { 
-		boardService.글쓰기(board, principal.getUser());
-		return new ResponseDto(HttpStatus.OK.value(), "글쓰기가 완료되었습니다.");
-	}
-	 */
 
 	
 	@RequestMapping("/reviewList.do")
--- meomum/src/main/webapp/WEB-INF/views/review/reviewWrite.jsp
+++ meomum/src/main/webapp/WEB-INF/views/review/reviewWrite.jsp
@@ -11,7 +11,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <!-- ckeditor -->
-<script src="https://cdn.ckeditor.com/4.21.0/standard/ckeditor.js"></script>
+<script src="resources/ckeditor/ckeditor.js"></script>
 
 
 <style>
