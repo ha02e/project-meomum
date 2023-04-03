3f72cc2e1ba5d6d8c127c242b97ed756abe5a74c
NamHyoJeong <xizu745@gmail.com>
2023-04-03 23:06:34 +0900

    상세 페이지 프론트 수정 / 장바구니 추가, 중복 확인

--- meomum/src/main/java/com/mm/cart/model/CartDAOImple.java
+++ meomum/src/main/java/com/mm/cart/model/CartDAOImple.java
@@ -1,5 +1,6 @@
 package com.mm.cart.model;
 
+import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 
 public class CartDAOImple implements CartDAO {
@@ -19,5 +20,22 @@ public class CartDAOImple implements CartDAO {
 		int count=sqlMap.insert("cartInsert",dto);
 		return count;
 	}
+	
+	
+	//장바구니 중복 확인
+	@Override
+	public List<CartDTO> cartCheck(int pro_idx) {
+		List<CartDTO> lists=sqlMap.selectList("cartCheck",pro_idx);
+		return lists;
+	}
+	
+	
+	//중복된 상품의 개수 추가
+	@Override
+	public int cartMoreInsert(CartDTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 
 }
