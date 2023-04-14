a3d57e96d03007c936015d85f72fba6071c3d997
DESKTOP-CH4GGAI\gram <wkdtn134@naver.com>
2023-04-15 00:14:28 +0900

    [2023.04.15] -point. js 수정

--- meomum/src/main/java/com/mm/point/model/PointDAO.java
+++ meomum/src/main/java/com/mm/point/model/PointDAO.java
@@ -4,11 +4,19 @@ import java.util.List;
 
 public interface PointDAO {
 	/* public PointDTO pointTotal(int user_idx); */
+	
+	/**포인트 조회*/
 	public int pointTotal(int user_idx);
+	/**포인트 사용 및 적립*/
 	public int pointInsert(PointDTO dto);
+	/**정리일상 결제 포인트 조회*/
+	public PointDTO svcPointSelect(String cate_idx);
+	
+	
 	public int pointInsertManager(PointDTO dto);
 	public List<PointDTO> pointList(int user_idx,int cp, int ls);
 	public int pointCnt(int user_idx);
+	
 	
 
 }
