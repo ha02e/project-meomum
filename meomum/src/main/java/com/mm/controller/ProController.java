38d5308af4527f2163f25e35aa65cdf2066ab0e7
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-09-08 00:33:18 +0900

    230908_@Async 어노테이션 import

--- meomum/src/main/java/com/mm/controller/ProController.java
+++ meomum/src/main/java/com/mm/controller/ProController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
